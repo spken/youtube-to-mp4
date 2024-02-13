53e71b46a898a3373e3eff818cee17e16c1f4fc1
spken <varematias11@gmail.com>
2024-02-13 10:22:33 +0100

    Make base template

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ This module contains a Flask application which completes
 the download of a youtube video as well as the querying of
 certain videos.
 """
+# https://pytube.io/en/latest/user/quickstart.html
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
-def index():
-    return "Hello, World"
-
-# https://pytube.io/en/latest/user/quickstart.html
+def index():    
+    """
+    Renders index on application launch
+    """
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
