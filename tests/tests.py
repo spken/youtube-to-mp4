327e3b10a239041ff6307857742b469433729a3e
spken <varematias11@gmail.com>
2024-02-20 14:19:39 +0100

    Add tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,33 +1,57 @@
 """
-This module contains tests for the app module.
+This module tests the app module.
 """
 
 import pytest
-from app import app
+from ..app import app as flask_app
+
+@pytest.fixture(name="app")
+def fixture_app():
+    """
+    Initalize flask application
+    """
+    flask_app.config.update({
+        "TESTING": True,
+    })
+    yield flask_app
 
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
+@pytest.fixture(name="client")
+def fixture_client(app):
+    """
+    Gets the testing client
+    """
+    return app.test_client()
 
 
 def test_index(client):
+    """
+    Tests the index GET
+    """
     response = client.get("/")
     assert response.status_code == 200
 
 
 def test_about(client):
+    """
+    Tests the about GET
+    """
     response = client.get("/about")
     assert response.status_code == 200
 
 
 def test_privacy(client):
+    """
+    Tests the privacy GET
+    """
     response = client.get("/privacy")
     assert response.status_code == 200
 
 
 def test_download(client):
+    """
+    Tests the video download
+    """
     url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     response = client.get("/download?url=" + url)
     assert response.status_code == 200
